3efd0b1fde270d67529bcb9c02bbe4c6fb0104ae
Ujang-shi <shi.gao@northeastern.edu>
2025-11-05 12:18:46 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+all books from different shelves
+[(3,1), (1,2), (5,3)]
+multiple books from the same shelf
+[(3,1),(3,24),(3,3)]
+Books are already in the correct order
+[(1,2),(2,3),(3,4)]
+books in reverse order
+[(3,1),(2,1),(1,1)]
 
 
