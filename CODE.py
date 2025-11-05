6328bcb7c7ca95079306a9f6731d9b8a1f0ce49d
Ujang-shi <shi.gao@northeastern.edu>
2025-11-05 12:19:28 -0800

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,1 +1,16 @@
-
+Algorithm SORT-BOOKS(books[1..n])
+  for i ← 2 to n do
+      key ← books[i]
+      j ← i - 1
+      while j ≥ 1 and (
+            books[j].shelfNumber > key.shelfNumber OR
+           (books[j].shelfNumber = key.shelfNumber AND
+            books[j].returnOrder > key.returnOrder)
+          ) do
+          books[j + 1] ← books[j]
+          j ← j - 1
+      end while
+      books[j + 1] ← key
+  end for
+  return books
+End Algorithm
